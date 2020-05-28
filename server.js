a7bf4193ad8ba48d3407431079a4a77f352137a2
Phelan <phelanmccall@yahoo.com>
2020-05-27 19:18:18 -0700

    server work

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
-const PORT = process.env.PORT || 9000;
-
+const PORT = process.env.PORT || 5000;
+app.use(express.static("client/build"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./build"));
+const router = require("express").Router();
+router.use("/", function (req, res) {
+      res.sendFile(path.join(__dirname, "./public/index.html"));
+    
+  });
+  router.use("/favicon.ico", function(req, res) {
+        res.sendFile(path.join(__dirname, "./public/favicon.ico"));
+   });
 
-app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-});
+app.use(router);
+
+app.listen(PORT, function() {
+    console.log(`Listening on port ${PORT}` );
+  }); 
