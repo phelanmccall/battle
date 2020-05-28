0292f2dc26bf12c5335e12f2c4bbb50ef2b3f872
Phelan <phelanmccall@yahoo.com>
2020-05-27 20:00:43 -0700

    trying to get it to work

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
-app.use(express.static("client/build"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static("./build"));
+app.use(express.static(path.join(__dirname, "public")));
 const router = require("express").Router();
 router.use("/", function (req, res) {
-      res.sendFile(path.join(__dirname, "./build/index.html"));
+      res.sendFile(path.join(__dirname, "./public/index.html"));
     
   });
   router.use("/favicon.ico", function(req, res) {
-        res.sendFile(path.join(__dirname, "./build/favicon.ico"));
+        res.sendFile(path.join(__dirname, "./public/favicon.ico"));
    });
 
 app.use(router);
